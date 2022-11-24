d538362df1f224c7b83aac9b2bb39798a5b41ac3
“ohachim” <othmanehachim@gmail.com>
2022-11-24 19:12:56 +0100

    parser

---
+++ parser.py
@@ -0,0 +1,64 @@
+
+import re
+
+class parser():
+
+    def __init__(self, path):
+        self._path = path
+
+    def loadData(self):
+        try:
+            with open(self._path, "r") as f:
+                self._lines = f.readlines()
+        except FileNotFoundError:
+            print("File not found.")
+            exit()
+        except OSError:
+            print("OS Error.")
+            exit()
+        except Exception as e:
+            print(f"Unexpected error {repr(e)}")
+            exit()
+
+    def printRawPuzzle(self):
+        for line in self._lines:
+            print(line, end="")
+
+    def removeComments(self):
+        commentPatten = re.compile("(#.+)|[\n]")
+        newLines = []
+        for line in self._lines:
+            newLine = re.sub(commentPatten, '', line)
+            if newLine:
+                newLines.append(newLine.strip())
+        return newLines
+
+    def getShape(self, newLines):
+        self._shape = int(newLines[0].split()[0])
+        if self._shape <= 2:
+            print("Error: Shape cannot be lower than 2.")
+            exit()
+
+    def flattenPuzzle(self, puzzleLines):
+        self._flattenedPuzzle = []
+        for line in puzzleLines:
+            self._flattenedPuzzle += [int(x) for x in line.split()]
+        for i in range(pow(self._shape, 2)):
+            if i not in self._flattenedPuzzle:
+                print("Error: Wrong numbers in puzzle.")
+                exit()
+
+    def cleanPuzzle(self):
+        newLines = self.removeComments()
+        if len(newLines[0].split()) != 1:
+            print("Error: Wrong format.")
+            exit()
+        self.getShape(newLines)
+        self.flattenPuzzle(newLines[1:])
+        
+if __name__ == "__main__":
+    p = parser("puzzle.txt")
+    p.loadData()
+    # p.printRawPuzzle()
+    p.cleanPuzzle()
+    print(p._flattenedPuzzle)
