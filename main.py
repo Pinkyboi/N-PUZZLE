5b5037d99a56cb69749669fc670a91d6e522efe2
Aymane Benaissa <47903494+Pinkyboi@users.noreply.github.com>
2022-11-28 03:18:19 +0100

    Add algorithm to program arguments

--- main.py
+++ main.py
@@ -2,33 +2,7 @@ from puzzle import PuzzleNode
 
 from puzzleSolver import PuzzleSolver
 from parser import Parser
-import argparse
-import os
-
-def heuristicCheck(heuristic):
-    if heuristic == 0:
-        heuristic = PuzzleSolver.manhatanDistance
-    elif heuristic == 1:
-        heuristic = PuzzleSolver.euclideanDistance
-    elif heuristic == 2:
-        heuristic = PuzzleSolver.misplacedTile
-    else:
-        print(f"heuristic:{heuristic} is not a valid heuristic")
-        exit()
-    
-def file_path(path):
-    if os.path.isfile(path):
-        return path
-    else:
-        print(f"readable_dir:{path} is not a valid path")
-        exit()
-
-def getExectionParameters():
-    parser = argparse.ArgumentParser()
-    parser.add_argument("--heuristic", type=heuristicCheck, default=0, help="The heuristic to use for the puzzle solver:-1: Manhattan Distance\n-2: Euclidean Distance\n-3: Misplaced Tile", required=False)
-    parser.add_argument("-v", action="store_true", default=False , help="Visualize the solution", required=False)
-    parser.add_argument("-p", "--path", type=file_path, help="Path to a puzzle file", required=True)
-    return parser.parse_args()
+from argumentHandler import getExectionParameters
 
 if __name__ == "__main__":
     import time 
@@ -45,7 +19,7 @@ if __name__ == "__main__":
     firstNode = PuzzleNode(start_puzzle, parserIstance.shape, None)
     endNode = PuzzleNode(end_puzzle, parserIstance.shape, None)
     # pes = PuzzleEndStates(firstNode.dim, snail)
-    solver = PuzzleSolver(firstNode, endNode, PuzzleSolver.manhatanDistance, PuzzleSolver.greedySearch)
+    solver = PuzzleSolver(firstNode, endNode, execParameters.heuristic, execParameters.algorithm)
     goal = solver.solve()
     print("{}".format(time.time() - start))
     goal.printNode()
