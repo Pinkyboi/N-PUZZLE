15b3872f6e7382e44340bb31f86024dd2a1665cc
Aymane Benaissa <47903494+Pinkyboi@users.noreply.github.com>
2022-11-28 03:26:49 +0100

    Specify end state from arguments

--- argumentHandler.py
+++ argumentHandler.py
@@ -10,7 +10,7 @@ def heuristicCheck(heuristic):
     elif heuristic == 'misplaced_tile':
         return PuzzleSolver.misplacedTile
     else:
-        print(f"Heuristic: {heuristic} is not a valid heuristic")
+        print(f"Heuristic: {heuristic} is not a valid heuristic.")
         exit()
 
 def seachAlgorithmCheck(algorithm):
@@ -21,7 +21,20 @@ def seachAlgorithmCheck(algorithm):
     elif algorithm == "Greedy":
         return PuzzleSolver.greedySearch
     else:
-        print(f"Algorithm: {algorithm} is not a valid search algorithm")
+        print(f"Algorithm: {algorithm} is not a valid search algorithm.")
+        exit()
+
+def goalGeneratorCheck(goal):
+    if goal == 'vertical':
+        return puzzleGoalGenerator.verticalGoalState
+    if goal == 'horizontal':
+        return puzzleGoalGenerator.horizontalGoalState
+    if goal == 'x_spiral':
+        return puzzleGoalGenerator.xMajorSpiralGoalState
+    if goal == 'y_spiral':
+        return puzzleGoalGenerator.yMajorSpiralGoalState
+    else:
+        print(f"Goal State: {goal} is not a valid goal state.")
         exit()
 
 def file_path(path):
@@ -35,6 +48,7 @@ def getExectionParameters():
     parser = argparse.ArgumentParser()
     parser.add_argument("--heuristic", type=heuristicCheck, default='manhattan_distance', help="The heuristic to use for the puzzle solver:-1: manhattan_distance\n-2: euclidean_distance\n-3: misplaced_tile", required=False)
     parser.add_argument('--algorithm', type=seachAlgorithmCheck, default='A*', help="The search algorithm to use for the puzzle solver:-1: A*\n-2: Djikstra\n-3: Greedy", required=False)
+    parser.add_argument('--goal', type=goalGeneratorCheck, default='x_spiral', help="The search algorithm to use for the puzzle solver:-1: vertical\n-2: horizontal\n-3: x_spiral\n-4: y_spiral", required=False)
     parser.add_argument("-v", action="store_true", default=False , help="Visualize the solution", required=False)
     parser.add_argument("-p", "--path", type=file_path, help="Path to a puzzle file", required=True)
     return parser.parse_args()
--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 from puzzle import PuzzleNode
-
 from puzzleSolver import PuzzleSolver
 from parser import Parser
 from argumentHandler import getExectionParameters
@@ -13,12 +12,8 @@ if __name__ == "__main__":
     parserIstance.loadData()
     parserIstance.cleanPuzzle()
     start_puzzle = parserIstance.flattenedPuzzle
-    # end_puzzle = [1, 2, 3, 4, 5, 16, 17, 18, 19, 6, 15, 24, 0, 20, 7, 14, 23, 22, 21, 8, 13, 12, 11, 10, 9]
-    # end_puzzle = [1,2,3,4,12,13,14,5,11,0,15,6,10,9,8,7]
-    end_puzzle = [1, 2, 3, 8, 0, 4, 7, 6, 5]
     firstNode = PuzzleNode(start_puzzle, parserIstance.shape, None)
-    endNode = PuzzleNode(end_puzzle, parserIstance.shape, None)
-    # pes = PuzzleEndStates(firstNode.dim, snail)
+    endNode = PuzzleNode(execParameters.goal(parserIstance.shape), parserIstance.shape, None)
     solver = PuzzleSolver(firstNode, endNode, execParameters.heuristic, execParameters.algorithm)
     goal = solver.solve()
     print("{}".format(time.time() - start))
